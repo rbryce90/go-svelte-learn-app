4f1b4bac91364b4732009a8fef2f54d9a85f68db
Richard Bond <richardbond@Richards-MBP.lan>
2024-09-08 23:12:30 -0500

    added new pages

---
+++ client/src/lib/Nav.svelte
@@ -0,0 +1,12 @@
+<nav>
+  <a href="/">Home</a>
+  <a href="/about">About</a>
+</nav>
+
+<style>
+  nav {
+    background-color: red;
+    padding: 0;
+    margin: 0;
+  }
+</style>
---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,24 @@
+<script>
+  import Nav from "$lib/Nav.svelte";
+</script>
+
+<Nav />
+
+<main>
+  <slot></slot>
+</main>
+
+<footer>this is a footer</footer>
+
+<style>
+  main {
+    margin: 0; /* Remove margin around the main content */
+    padding: 0; /* Remove padding around the main content */
+    min-height: 100vh; /* Ensure main content takes up at least full viewport height */
+  }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+</style>
